0870ad208100aa168f247f517e4adacb60e375e4
MuhammadTalha786786 <mtalha25800@gmail.com>
2023-02-20 14:50:21 +0500

    initial commit

---
+++ pages/utils.tsx
@@ -0,0 +1,12 @@
+import { Poppins } from "@next/font/google";
+
+export const regular = Poppins({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export const medium = Poppins({
+  weight: "600",
+  subsets: ["latin"],
+});
+
